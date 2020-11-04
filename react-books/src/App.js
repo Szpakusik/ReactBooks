6eeffafdf35e8362b9e30b2f887234c0f1224717
Szpakusik <szpakusik@gmail.com>
2020-11-04 20:15:28 +0100

    init

---
+++ react-books/src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import {
+  Navbar,
+  Row,
+  Col
+} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App bg-dark">
+      <Navbar className="bg-dark text-center" expand="md">qwe</Navbar>
+      <Row></Row>
+    </div>
+  );
+}
+
+export default App;
