be2099da3b6d6fd6888021234ead5e461c69bbff
Szpakusik <szpakusik@gmail.com>
2020-11-08 19:36:22 +0100

    State handling

--- react-books/src/Containers/SearchContainer.jsx
+++ react-books/src/Containers/SearchContainer.jsx
@@ -6,18 +6,35 @@ import Search from "../Components/Search"
 function SearchContainer({ findBooks, books }) {
 
   const [search, setSearch] = useState("");
+  const [author, setAuthor] = useState("");
+  const [language, setlanguage] = useState("");
 
+  const [toDate, setToDate] = useState("");
+  const [fromDate, setFromDate] = useState("");
+
+  const handleQueryChange = e => setSearch(e.target.value);
+  const handleAuthorChange = e => setAuthor(e.target.value);
+  const handleFromDateChange = e => setFromDate(e.target.value);
+  const handleToDateChange = e => setToDate(e.target.value);
+  
   const handleSearch = () => {
-    findBooks(search)
+    findBooks({
+      search,
+      author,
+      language,
+      fromDate,
+      toDate
+    })
   }
-  const handleChange = e => setSearch(e.target.value);
 
   return (
     <>
       <Search
-        books={books}
-        handleChange={handleChange}
-        handleSearch={handleSearch} />
+        handleSearch={handleSearch}
+        handleQueryChange={handleQueryChange}
+        handleAuthorChange={handleAuthorChange}
+        handleFromDateChange={handleFromDateChange}
+        handleToDateChange={handleToDateChange} />
     </>
   )
 }
